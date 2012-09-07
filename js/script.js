e85a1e54021213bac987f292d19a62cb92b5904e
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2012-09-07 16:35:29 -0400

    still testing Select2 implementation

--- js/script.js
+++ js/script.js
@@ -122,9 +122,16 @@ $("form").submit(function(event) {
 	var stUrl = 'https://nickchamberlain.cartodb.com/api/v1/sql/?format=json&q=' + stQry + '&callback=?';
 
 	$("#street").select2({
-		ajax: {
-			url: stUrl,
-			dataType: 'jsonp'
+		placeholder: "Search for a movie",
+		minimumInputLength: 3,
+		query:  function (query) {
+					var data = {results: []}, i, j, s;
+					for (i = 1; i < 5; i++) {
+					s = "";
+					for (j = 0; j < i; j++) {s = s + query.term;}
+					data.results.push({id: query.term + i, text: s});
+					}
+				query.callback(data);
 			}
 	});
 
